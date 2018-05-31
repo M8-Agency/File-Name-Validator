568ec67ad8e071ca31f64ceb76479353203bdd4c
Yohana Verde <yohana.verde@m8agency.com>
2018-05-31 07:42:40 -0400

    app switch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import '../styles/app.css';
-import NameGenerator from "../components/NameGenerator";
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import CreativeSpecs from "../components/CreativeSpecs";
+import { Creative } from "../components/CreativeName";
+import "../styles/app.css";
+
+function NoMatch() {
+  return <div>WHOOPS!</div>;
+}
 
 class App extends Component {
   render() {
     return (
       <main>
         <h1>M8 Name Validator</h1>
-        <NameGenerator />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={CreativeSpecs} />
+            <Route path="/creative/" component={Creative} />
+            <Route component={NoMatch} />
+          </Switch>
+        </BrowserRouter>
       </main>
     );
   }

a090848b620ce8d37d033aa18a8cec5b4f0df565
Yohana Verde <yohana.verde@m8agency.com>
2018-06-05 15:32:58 -0400

    data values update

--- src/data/clients.js
+++ src/data/clients.js
@@ -2,7 +2,7 @@ const clients = [
   { value: "vfl", label: "Visit Florida" },
   { value: "cpa", label: "Copa Airlines" },
   { value: "htz", label: "Hertz" },
-  { value: "other", label: "Other" }
+  { value: "aaa", label: "Other" }
 ];
 
 export default clients;
--- src/data/platforms.js
+++ src/data/platforms.js
@@ -1,6 +1,6 @@
 const platform = [
   {},
-  { value: "fb + ig", label: "Facebook + Instagram" },
+  { value: "fb+ig", label: "Facebook + Instagram" },
   { value: "fb", label: "Facebook" },
   { value: "ig", label: "Instagram" },
   { value: "tw", label: "Twitter" },

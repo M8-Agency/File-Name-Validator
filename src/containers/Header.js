132a6d40928b9d3a1d59f576c106c62bd8d79db8
Yohana Verde <yohana.verde@m8agency.com>
2018-06-06 17:38:18 -0400

    class => className

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,22 +4,24 @@ import { Link } from "react-router-dom";
 class Header extends Component {
   render() {
     return (
-      <header class="mui-appbar mui--z1">
-        <div class="mui-container">
+      <header className="mui-appbar mui--z1">
+        <div className="mui-container">
           <table>
-            <tr class="mui--appbar-height">
-              <td class="mui--text-title">M8 Name Validator</td>
-              <td class="mui--text-right">
-                <ul class="mui-list--inline mui--text-body2">
-                  <li>
-                    <Link to={"/"}>Home</Link>
-                  </li>
-                  <li>
-                    <Link to={"/"}>Help</Link>
-                  </li>
-                </ul>
-              </td>
-            </tr>
+            <tbody>
+              <tr className="mui--appbar-height">
+                <td className="mui--text-title">M8 Name Validator</td>
+                <td className="mui--text-right">
+                  <ul className="mui-list--inline mui--text-body2">
+                    <li>
+                      <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                      <Link to={"/"}>Help</Link>
+                    </li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </header>

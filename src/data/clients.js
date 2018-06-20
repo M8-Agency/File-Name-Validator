58ace14a3d52290f4d09142a66f7e4e550299333
Yohana Verde <yohana.verde@m8agency.com>
2018-06-20 13:37:44 -0400

    adding sprint as client

--- src/data/clients.js
+++ src/data/clients.js
@@ -2,6 +2,7 @@ const clients = [
   { value: "vfl", label: "Visit Florida" },
   { value: "cpa", label: "Copa Airlines" },
   { value: "htz", label: "Hertz" },
+  { value: "spr", label: "Sprint" },
   { value: "aaa", label: "Other" }
 ];
 

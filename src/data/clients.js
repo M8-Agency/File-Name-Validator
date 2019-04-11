c7424c0f7ded1dc20cc942919b7bc162329cd763
Yohana Verde <yohana.verde@m8agency.com>
2019-04-10 20:24:21 -0400

    add new client - PMI

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,10 +1,11 @@
 const clients = [
-  { value: "vfl", label: "Visit Florida" },
-  { value: "cpa", label: "Copa Airlines" },
-  { value: "htz", label: "Hertz" },
-  { value: "spr", label: "Sprint" },
-  { value: "kfc", label: "KFC" },
-  { value: "aaa", label: "Other" }
+	{ value: "vfl", label: "Visit Florida" },
+	{ value: "pmi", label: "Phillip Morris International" },
+	{ value: "cpa", label: "Copa Airlines" },
+	{ value: "htz", label: "Hertz" },
+	{ value: "spr", label: "Sprint" },
+	{ value: "kfc", label: "KFC" },
+	{ value: "aaa", label: "Other" }
 ];
 
 export default clients;

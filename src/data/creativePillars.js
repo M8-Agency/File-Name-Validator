dab55b767ddabb35c4fbc39d56e837ba944b8c5b
Yohana Verde <yohana.verde@m8agency.com>
2019-04-10 20:25:01 -0400

    add new pillar for new client - PMI

--- src/data/creativePillars.js
+++ src/data/creativePillars.js
@@ -1,82 +1,93 @@
 function getCreativePillar(client) {
-  switch (client) {
-    case "htz":
-      return creativePillarHTZ;
-    case "vfl":
-      return creativePillarVFL;
-    case "spr":
-      return creativePillarSPR;
-    case "kfc":
-      return creativePillarKFC;
-    case "cpa":
-      return creativePillarCPA;
-    default:
-      return [];
-  }
+	switch (client) {
+		case "htz":
+			return creativePillarHTZ;
+		case "vfl":
+			return creativePillarVFL;
+		case "spr":
+			return creativePillarSPR;
+		case "kfc":
+			return creativePillarKFC;
+		case "cpa":
+			return creativePillarCPA;
+		case "pmi":
+			return creativePillarPMI;
+		default:
+			return [];
+	}
 }
 
 const creativePillarHTZ = [
-  {},
-  { value: "upto%off", label: "up to % off" },
-  { value: "%off", label: "% off" },
-  { value: "upto$off", label: "up to $off" },
-  { value: "$off", label: "$off" },
-  { value: "seasonal", label: "seasonal" },
-  { value: "membership", label: "membership" },
-  { value: "benefits", label: "benefits" },
-  { value: "car", label: "car" },
-  { value: "promo", label: "promo" },
-  { value: "generic", label: "generic" },
-  { value: "other", label: "Other" }
+	{},
+	{ value: "upto%off", label: "up to % off" },
+	{ value: "%off", label: "% off" },
+	{ value: "upto$off", label: "up to $off" },
+	{ value: "$off", label: "$off" },
+	{ value: "seasonal", label: "seasonal" },
+	{ value: "membership", label: "membership" },
+	{ value: "benefits", label: "benefits" },
+	{ value: "car", label: "car" },
+	{ value: "promo", label: "promo" },
+	{ value: "generic", label: "generic" },
+	{ value: "other", label: "Other" }
 ];
 
 const creativePillarVFL = [
-  {},
-  { value: "impulse", label: "Impulse" },
-  { value: "adventure", label: "Adventure" },
-  { value: "winter", label: "Winter" },
-  { value: "summer", label: "Summer" },
-  { value: "fall", label: "Fall" },
-  { value: "spring", label: "Spring" },
-  { value: "family-hispanic", label: "Family Hispanic" },
-  { value: "family-african-american", label: "Family African American" },
-  { value: "family", label: "Family" },
-  { value: "experience", label: "Experience" },
-  { value: "international", label: "International" },
-  { value: "partner", label: "Partner" }
+	{},
+	{ value: "impulse", label: "Impulse" },
+	{ value: "adventure", label: "Adventure" },
+	{ value: "winter", label: "Winter" },
+	{ value: "summer", label: "Summer" },
+	{ value: "fall", label: "Fall" },
+	{ value: "spring", label: "Spring" },
+	{ value: "family-hispanic", label: "Family Hispanic" },
+	{ value: "family-african-american", label: "Family African American" },
+	{ value: "family", label: "Family" },
+	{ value: "experience", label: "Experience" },
+	{ value: "international", label: "International" },
+	{ value: "partner", label: "Partner" }
 ];
 
 const creativePillarCPA = [
-  {},
-  { value: "bm", label: "Be More" },
-  { value: "fb", label: "Feel Better" },
-  { value: "gp", label: "Go Places" },
-  { value: "other", label: "Other" }
+	{},
+	{ value: "bm", label: "Be More" },
+	{ value: "fb", label: "Feel Better" },
+	{ value: "gp", label: "Go Places" },
+	{ value: "other", label: "Other" }
 ];
 
 const creativePillarSPR = [
-  {},
-  { value: "rateplans", label: "Rate Plans" },
-  { value: "phonesdeviceoffers", label: "Phones/Device Offers" },
-  { value: "addons", label: "Add-Ons" },
-  { value: "sponsorships", label: "Sponsorships" },
-  { value: "webteleexclusives", label: "Web/Tele Exclusives" },
-  { value: "other", label: "Other" }
+	{},
+	{ value: "rateplans", label: "Rate Plans" },
+	{ value: "phonesdeviceoffers", label: "Phones/Device Offers" },
+	{ value: "addons", label: "Add-Ons" },
+	{ value: "sponsorships", label: "Sponsorships" },
+	{ value: "webteleexclusives", label: "Web/Tele Exclusives" },
+	{ value: "other", label: "Other" }
 ];
 
 const creativePillarKFC = [
-  {},
-  { value: "mc-innovation", label: "MC Innovation" },
-  { value: "mc-uniquemoments", label: "MC Uniquemoments" },
-  { value: "mc-trueyou", label: "MC True You" },
-  { value: "va-money", label: "VA Money" },
-  { value: "va-time", label: "VA Time" },
-  { value: "va-food", label: "VA Food" },
-  { value: "fs-foodprep", label: "FS Foodprep" },
-  { value: "fs-secretrecipe", label: "FS Secret Recipe" },
-  { value: "fs-colonel", label: "FS Colonel" },
-  { value: "campaign", label: "Campaign" },
-  { value: "other", label: "Other" }
+	{},
+	{ value: "mc-innovation", label: "MC Innovation" },
+	{ value: "mc-uniquemoments", label: "MC Uniquemoments" },
+	{ value: "mc-trueyou", label: "MC True You" },
+	{ value: "va-money", label: "VA Money" },
+	{ value: "va-time", label: "VA Time" },
+	{ value: "va-food", label: "VA Food" },
+	{ value: "fs-foodprep", label: "FS Foodprep" },
+	{ value: "fs-secretrecipe", label: "FS Secret Recipe" },
+	{ value: "fs-colonel", label: "FS Colonel" },
+	{ value: "campaign", label: "Campaign" },
+	{ value: "other", label: "Other" }
+];
+
+const creativePillarPMI = [
+	{},
+	{ value: "convenience", label: "Convenience" },
+	{ value: "vanity", label: "Vanity" },
+	{ value: "social", label: "Social" },
+	{ value: "lifestyle", label: "Lifestyle" },
+	{ value: "other", label: "Other" }
 ];
 
 export default { getCreativePillar };
--- src/data/creativeTypes.js
+++ src/data/creativeTypes.js
@@ -1,58 +1,57 @@
 function getCreativeType(initiative) {
-  switch (initiative) {
-    case "social":
-      return creativeTypeSocial;
-    case "display":
-      return creativeTypeDisplay;
-
-    default:
-      return creativeTypeOther;
-  }
+	switch (initiative) {
+		case "social":
+			return creativeTypeSocial;
+		case "display":
+			return creativeTypeDisplay;
+		default:
+			return creativeTypeOther;
+	}
 }
 
 const creativeTypeDisplay = [
-  {},
-  { value: "html5", label: "Html5" },
-  { value: "video", label: "Video" },
-  { value: "audio", label: "Audio" },
-  { value: "image", label: "Image" },
-  { value: "gif", label: "Gif" },
-  { value: "image+text", label: "Image + text" },
-  { value: "email", label: "Email" },
-  { value: "copy", label: "Copy" },
-  { value: "dynamic", label: "Dynamic" },
-  { value: "richmedia", label: "Rich media" },
-  { value: "custom", label: "Custom" }
+	{},
+	{ value: "html5", label: "Html5" },
+	{ value: "video", label: "Video" },
+	{ value: "audio", label: "Audio" },
+	{ value: "image", label: "Image" },
+	{ value: "gif", label: "Gif" },
+	{ value: "image+text", label: "Image + text" },
+	{ value: "email", label: "Email" },
+	{ value: "copy", label: "Copy" },
+	{ value: "dynamic", label: "Dynamic" },
+	{ value: "richmedia", label: "Rich media" },
+	{ value: "custom", label: "Custom" }
 ];
 
 const creativeTypeOther = [
-  {},
-  { value: "page", label: "Page" },
-  { value: "deck", label: "Deck" },
-  { value: "html5", label: "Html5" },
-  { value: "video", label: "Video" },
-  { value: "audio", label: "Audio" },
-  { value: "image", label: "Image" },
-  { value: "gif", label: "Gif" },
-  { value: "image+text", label: "Image + text" },
-  { value: "email", label: "Email" },
-  { value: "copy", label: "Copy" },
-  { value: "dynamic", label: "Dynamic" },
-  { value: "richmedia", label: "Rich media" },
-  { value: "custom", label: "Custom" }
+	{},
+	{ value: "page", label: "Page" },
+	{ value: "deck", label: "Deck" },
+	{ value: "html5", label: "Html5" },
+	{ value: "video", label: "Video" },
+	{ value: "audio", label: "Audio" },
+	{ value: "image", label: "Image" },
+	{ value: "gif", label: "Gif" },
+	{ value: "image+text", label: "Image + text" },
+	{ value: "email", label: "Email" },
+	{ value: "copy", label: "Copy" },
+	{ value: "dynamic", label: "Dynamic" },
+	{ value: "richmedia", label: "Rich media" },
+	{ value: "custom", label: "Custom" }
 ];
 
 const creativeTypeSocial = [
-  {},
-  { value: "carousel", label: "Carousel" },
-  { value: "image", label: "Image" },
-  { value: "gif", label: "Gif" },
-  { value: "video", label: "Video" },
-  { value: "slideshow", label: "Slideshow" },
-  { value: "leadform", label: "Leadform" },
-  { value: "page", label: "Page" },
-  { value: "canvas", label: "Canvas" },
-  { value: "dynamic", label: "Dynamic" }
+	{},
+	{ value: "carousel", label: "Carousel" },
+	{ value: "image", label: "Image" },
+	{ value: "gif", label: "Gif" },
+	{ value: "video", label: "Video" },
+	{ value: "slideshow", label: "Slideshow" },
+	{ value: "leadform", label: "Leadform" },
+	{ value: "page", label: "Page" },
+	{ value: "canvas", label: "Canvas" },
+	{ value: "dynamic", label: "Dynamic" }
 ];
 
 export default getCreativeType;
